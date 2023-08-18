eeb3050d1cf033b54c11b5ee57ddbfffeb0480fd
Williany <willianyso@gmail.com>
2023-08-18 15:32:03 -0300

    [modularização] Função saque

--- Sistema bancario/sistema.py
+++ Sistema bancario/sistema.py
@@ -1,4 +1,5 @@
 from deposito import depo
+from saque import saq
 saldo = 0
 deposito = ""
 saques = 0
@@ -14,24 +15,15 @@ while True:
           q = Sair''')
     operacao = input()
     if operacao == "d":
-        saldoAtual = depo(saldo, extrato)  
+        saldoAtual, extrato = depo(saldo, extrato)  
         extrato = extrato + saldoAtual[1]
         saldo = saldo +  saldoAtual[0]
     if operacao == "s": 
-        if saques < 3:
-            print("Qual valor deseja sacar?")
-            s = float(input())
-            if (s <= saldo) and (s<=500):
-                saldo = saldo - s
-                print(f"Saque realizado. Novo saldo: {saldo}")
-                extrato = extrato + (f'''Foi realizado um saque no valor de {s:.2f}
-''')
-                saques = saques + 1
-                print(saques)
-            else:
-                print("Não á limite disponível para o saldo")
-        else:
-            print("O número de saques diários permitidos foi alcançado")    
+         saque = saq(saldo=saldo,
+                          saques=saques,
+                          limite_saque = LIMITE_SAQUE,
+                          extrato = extrato
+                          )
     if operacao == "e":
         if extrato == "":
             print("Ainda não foi realizada nenhuma operação")
